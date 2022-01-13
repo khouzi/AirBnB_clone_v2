da3418095ad71096d5ffca299ed4b5d3ffcee7ac
khouzi <khouzem.boussaid@gmail.com>
2022-01-13 12:12:46 +0100

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get isntall -y nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Hello" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '40i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+# Bash Script to Setup Airbnb Static_web 
+sudo apt-get update -y
+sudo apt-get updgrade -y
+sudo apt-get install -y nginx
+mkdir -p  /data/web_static/
+mkdir -p /data/web_static/releases/test/
+mkdir /data/web_static/shared/
+echo "Hello AirBnb" | sudo tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current 
+chown ubuntu:ubuntu -hR /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
